3dcae15927576fc02f7ec7348d2ae6635689674f
Deyvid silva <silvadeyvid017@gmail.com>
2024-09-25 15:34:19 -0300

    subindo projeto

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const nav = document.querySelector(".nav");
+
+document.querySelector("#menu-icon").addEventListener("click", () => {
+  nav.style.display = nav.style.display === "block" ? "none" : "block";
+  scroll();
+});
+
+function scroll() {
+  if (nav.style.display == "block") {
+    document.querySelector("body").style.overflow = "hidden";
+    return;
+  }
+  document.querySelector("body").style.overflow = "visible";
+}
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 680) nav.style.display = "none";
+});
